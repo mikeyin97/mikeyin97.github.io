e11ce15ae0f085cdbcb726ba495f81b2b7a44f65
mikeyin97 <yinjitong@hotmail.com>
2016-10-29 13:40:09 -0400

    basically done

--- port2.js
+++ port2.js
@@ -194,4 +194,18 @@ $(document).ready(function(){
         } 
     });
 
+
+    $(window).scroll(function(){
+		if ($(this).scrollTop() > 150) {
+            $('.scrollToTop').fadeIn();
+		} else {
+			$('.scrollToTop').fadeOut();
+		}
+	});
+	
+	//Click event to scroll to top
+	$('.scrollToTop').click(function(){
+		$('html, body').animate({scrollTop : 0},800);
+		return false;
+	});
 });
